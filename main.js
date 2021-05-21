ae48c948c017979053c4b7fd137555f7ede28e3d
ibraakhimoof <ibrakhimoof@yandex.com>
2021-05-21 17:04:31 +0300

    logic

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
-window.onload = function() {
-    document.querySelector('.header-burger').addEventListener('click', function() {
-        document.querySelector('.header-burger').classList.toggle('active')
-        document.querySelector('.navigation').classList.toggle('active')
-        document.querySelector('.info-bar').classList.toggle('active')
-        document.querySelector('#uzlang').innerText = 'Uzbek'
-        document.querySelector('#rulang').innerText = 'Russian'
-        document.querySelector('#enlang').innerText = 'English'
-    })
+function handleWindowLoad () {
+   const newBurger =  document.querySelector('.header-burger') ;
+   newBurger.addEventListener('click', burgermenu );
+
 }
+function burgermenu() {
+    document.querySelector('.header-burger').classList.toggle('active')
+    document.querySelector('.navigation').classList.toggle('active')
+    document.querySelector('.info-bar').classList.toggle('active')
+    document.querySelector('#uzlang').innerText = 'Uzbek'
+    document.querySelector('#rulang').innerText = 'Russian'
+    document.querySelector('#enlang').innerText = 'English'
+}
+
+function createDynamicElements() {
+    const secondaryContainer = document.createElement('section')
+    secondaryContainer.classList.add('secondary-container')
+
+    const cardInfo = document.createElement('div')
+    cardInfo.classList.add('card-info')
+    secondaryContainer.append(cardInfo)
+
+    const imgCard = document.createElement ('div')
+    imgCard.classList.add('img-card')
+    cardInfo.append(imgCard)
+
+    const imgElement = document.createElement('img')
+    imgElement.src = " "
+    tagElement.append(imgElement)
+
+    const tagElement = document.createElement('a')
+    bigCard.append(tagElement)
+
+
+
+
+
+}
+
+window.addEventListener('load' , handleWindowLoad ); 
