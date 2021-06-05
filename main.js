4d5f05ab8aac913c6a1b84ef1d67cb2f938a1aef
ibraakhimoof <ibrakhimoof@yandex.com>
2021-06-05 18:45:56 +0500

    create dynamic elements

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const IMAGE_URL = "http://192.144.37.95/images/";
 function handleWindowLoad () {
    const newBurger =  document.querySelector('.header-burger') ;
    newBurger.addEventListener('click', burgermenu );
@@ -19,49 +20,78 @@ async function getFetchApi(){
         
         console.log(loadElements)   
         
+        
+        
         for (let index = 0; index < 3; index++) {
-            const items = leadElements[index];
-            console.log(loadElements) 
+            createDynamicElements(loadElements[index])
+            secondInfobloks(loadElements[index])
         }
     }
     catch (e){
         console.log(e)
     }
 }
+//timestamp conversion
+// function convertTimestamp(timestamp) {
+//     var d = new Date(timestamp * 1000), // Convert the passed timestamp to milliseconds
+//         yyyy = d.getFullYear(),
+//         mm = ('0' + (d.getMonth() + 1)).slice(-2),  // Months are zero based. Add leading 0.
+//         dd = ('0' + d.getDate()).slice(-2),         // Add leading 0.
+//         hh = d.getHours(),
+//         h = hh,
+//         min = ('0' + d.getMinutes()).slice(-2),     // Add leading 0.
+//         ampm = 'AM',
+//         time;
 
-function createDynamicElements() {
-    const secondaryContainer = document.createElement('section')
-    secondaryContainer.classList.add('secondary-container')
+//     if (hh > 12) {
+//         h = hh - 12;
+//         ampm = 'PM';
+//     } else if (hh === 12) {
+//         h = 12;
+//         ampm = 'PM';
+//     } else if (hh == 0) {
+//         h = 12;
+//     }
 
-    const cardInfo = document.createElement('div')
-    cardInfo.classList.add('card-info')
+//     // ie: 2014-03-24, 3:00 PM
+//     time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
+//     return time;
+// }
+
+function createDynamicElements(data) {
+    const secondaryContainer = document.getElementById('secondryContainer')
+
+    const cardInfo = document.createElement('a')
+    
+    cardInfo.classList.add('card')
+    cardInfo.setAttribute("href" , " ")
     secondaryContainer.append(cardInfo)
 
     const imgCard = document.createElement ('div')
-    imgCard.classList.add('img-card')
+    imgCard.classList.add('card-image-container')
     cardInfo.append(imgCard)
 
-    const imgElement = document.createElement('img')
-    imgElement.src = " "
-    tagElement.append(imgElement)
 
-    const tagElement = document.createElement('a')
-    bigCard.append(tagElement)
+    const imgElement = document.createElement('img')
+    imgElement.src = IMAGE_URL + data.image;
+    imgElement.classList.add('card-image-src')
+    imgCard.append(imgElement)
 
     const selectBtn = document.createElement('button')
     selectBtn.classList.add('selected-btn')
-    tagElement.append(selectBtn)
+    selectBtn.innerText = ('Category')
+    imgCard.append(selectBtn)
 
     const infoDivElement = document.createElement('div')
     infoDivElement.classList.add('info-element')
     cardInfo.append(infoDivElement)
 
     const mainInfoElement = document.createElement('h4')
-    mainInfoElement.innerText('')
+    mainInfoElement.innerText = data.title
     infoDivElement.append(mainInfoElement)
 
     const mainPelements = document.createElement('p')
-    mainPelements.innerText('')
+    mainPelements.innerHTML = data.title
     infoDivElement.append(mainPelements)
 
     const brElement = document.createElement('br')
@@ -69,9 +99,43 @@ function createDynamicElements() {
 
     const timeinfo = document.createElement('span')
     timeinfo.classList.add('time')
-    timeinfo.innerText('')
+    timeinfo.innerText = data.date
     infoDivElement.append(timeinfo)
 
+}
 
+function secondInfobloks(data) {
+   const secondBlok = document.getElementById('second-blok')
+
+   const mainDiv = document.createElement('div')
+   mainDiv.classList.add('more-info-bloks')
+   secondBlok.append(mainDiv)
+
+   const titleName = document.createElement( 'h4')
+   titleName.innerText = 'Boshqa maqolalar'
+   mainDiv.append(titleName)
+
+   const articleElement = document.createElement('article')
+   mainDiv.append(articleElement)
+
+   const pElement = document.createElement('P')
+   pElement.innerText = data.title;
+   articleElement.append(pElement)
+
+   const timeElement = document.createElement('span')
+   timeElement.innerText = data.date
+   articleElement.append(timeElement)
+
+   const hrElement = document.createElement('hr')
+   mainDiv.append(hrElement)
+
+   const buttonAElement = document.createElement('a')
+   buttonAElement.setAttribute("href" , "")
+   mainDiv.append(buttonAElement)
+
+//    const buttonElement = document.createElement('button')
+//    buttonAElement.innerText = ("Barchasi")
+//    buttonAElement.append(buttonElement)
+    
 }
 window.addEventListener('load' , handleWindowLoad ); 
